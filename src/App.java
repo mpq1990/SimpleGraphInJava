5af1d921ad8403fd7159ecc13034a66bcaf3d3b7
Majid Qureshi <mpq1990@gmail.com>
2017-11-05 15:05:40 +0100

    basic graph

--- src/App.java
+++ src/App.java
@@ -3,7 +3,13 @@ public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Graph graph = new Graph(3);
+		graph.addEdge(2, 1);
+		graph.addEdge(2, 0);
+		Object[] obj = graph.adj(2);
+		for(int i = 0; i < obj.length; i++){
+			System.out.println(obj[i]);
+		}
 	}
 
 }
